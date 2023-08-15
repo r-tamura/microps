90ef18a209a65e989edbf4ea91ddd7757837a5b9
Ryoya Tamura <rtamura30@gmail.com>
2023-08-15 22:38:44 +0900

    step4 プロトコルの管理
    - ネットワーク層のプロトコル管理
    - プロトコルごとにキューを用意し、デバイスをの割り込みハンドラから送信メッセージのエントリを登録する

--- net.h
+++ net.h
@@ -23,6 +23,11 @@
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "up" : "down")
 
+/* struct net_protocolのtype */
+#define NET_PROTOCOL_TYPE_IP 0x0800
+#define NET_PROTOCOL_TYPE_ARP 0x0806
+#define NET_PROTOCOL_TYPE_IPV6 0x86dd
+
 struct net_device
 {
   struct net_device *next; // 次のデバイスへのポインタ
@@ -70,7 +75,9 @@ extern int
 net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 
 extern int
-net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev);
+net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t len, struct net_device *dev));
+
+extern int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev);
 
 extern int
 net_run(void);
